63d6b5d36097cab10b36e665732af40472fcdaa1
wain-aina <jesse.wainaina@student.moringaschool.com>
2021-06-11 18:08:31 +0300

    Final js commit

--- static/js/index.js
+++ static/js/index.js
@@ -11,33 +11,33 @@ function dateakan() {
   var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   if (gender == "male" && result == 0) {
-      alert(males[0]);
+      alert("Your akan name is " + males[0]);
     } else if (gender == "male" && result == 1) {
-      alert(males[1]);
+      alert("Your akan name is " + males[1]);
     } else if (gender == "male" && result == 2) {
-      alert(males[2]);
+      alert("Your akan name is " + males[2]);
     } else if (gender == "male" && result == 3) {
-      alert(males[3]);
+      alert("Your akan name is " + males[3]);
     } else if (gender == "male" && result == 4) {
-      alert(males[4]);
+      alert("Your akan name is " + males[4]);
     } else if (gender == "male" && result == 5) {
-      alert(males[5]);
+      alert("Your akan name is " + males[5]);
     } else if (gender == "male" && result == 6) {
-      alert(males[6]);
+      alert("Your akan name is " + males[6]);
     } else if (gender == "female" && result == 0) {
-      alert(females[0]);
+      alert("Your akan name is " + females[0]);
     } else if (gender == "female" && result == 1) {
-      alert(females[1]);
+      alert("Your akan name is " + females[1]);
     } else if (gender == "female" && result == 2) {
-      alert(females[2]);
+      alert("Your akan name is " + females[2]);
     } else if (gender == "female" && result == 3) {
-      alert(females[3]);
+      alert("Your akan name is " + females[3]);
     } else if (gender == "female" && result == 4) {
-      alert(females[4]);
+      alert("Your akan name is " + females[4]);
     } else if (gender == "female" && result == 5) {
-      alert(females[5]);
+      alert("Your akan name is " + females[5]);
     } else if (gender == "female" && result == 6) {
-      alert(females[6]);
+      alert("Your akan name is " + females[6]);
     } else {
       alert("invalid. Please try again");
     }
