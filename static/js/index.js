d780c9f08c4cdfddbae2842603fdc76e0ff3877c
wain-aina <jesse.wainaina@student.moringaschool.com>
2021-06-11 16:56:57 +0300

    This is the final js version

--- static/js/index.js
+++ static/js/index.js
@@ -1,49 +1,45 @@
-function day(cc, yy, mm, dd) {
-  return (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) mod 7
+function day (cc,yy,mm,dd){
+  return ( ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7) - 1;
 }
-
 function dateakan() {
   var cc = (document.getElementById("Id1").value);
   var yy = (document.getElementById("Id2").value);
   var mm = (document.getElementById("Id3").value);
   var dd = (document.getElementById("Id4").value);
-  var gender = (document.getElementById("Id5").value);
-  var result = day(cc, yy, mm, dd).toFixed();
-  let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  let female = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"];
-  if (day > 31 || day <= 0) {
-    alert("Invalid day!");
-  } else if (month > 12 || month <= 0) {
-    alert("Invalid month");
-  } else if (gender === "male" && result === 0) {
-    alert("Your Akan name is " + male[0]);
-  } else if (gender === "male" && result === 1) {
-    alert("Your Akan name is " + male[1]);
-  } else if (gender === "male" && result === 2) {
-    alert("Your Akan name is " + male[2]);
-  } else if (gender === "male" && result === 3) {
-    alert("Your Akan name is " + male[3]);
-  } else if (gender === "male" && result === 4) {
-    alert("Your Akan name is " + male[4]);
-  } else if (gender === "male" && result === 5) {
-    alert("Your Akan name is " + male[5]);
-  } else if (gender === "male" && result === 6) {
-    alert("Your Akan name is " + male[6]);
-  } else if (gender === "female" && result === 0) {
-    alert("Your Akan name is " + female[0]);
-  } else if (gender === "female" && result === 1) {
-    alert("Your Akan name is " + female[1]);
-  } else if (gender === "female" && result === 2) {
-    alert("Your Akan name is " + female[2]);
-  } else if (gender === "female" && result === 3) {
-    alert("Your Akan name is " + female[3]);
-  } else if (gender === "female" && result === 4) {
-    alert("Your Akan name is " + female[4]);
-  } else if (gender === "female" && result === 5) {
-    alert("Your Akan name is " + female[5]);
-  } else if (gender === "female" && result === 6) {
-    alert("Your Akan name is " + female[6]);
-  } else {
-    alert("null");
-  }
+  var gender=(document.getElementById("Id5").value);
+  var result=day(cc,yy,mm,dd).toFixed();
+  var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  if (gender == "male" && result == 0) {
+      alert(males[0]);
+    } else if (gender == "male" && result == 1) {
+      alert(males[1]);
+    } else if (gender == "male" && result == 2) {
+      alert(males[2]);
+    } else if (gender == "male" && result == 3) {
+      alert(males[3]);
+    } else if (gender == "male" && result == 4) {
+      alert(males[4]);
+    } else if (gender == "male" && result == 5) {
+      alert(males[5]);
+    } else if (gender == "male" && result == 6) {
+      alert(males[6]);
+    } else if (gender == "female" && result == 0) {
+      alert(females[0]);
+    } else if (gender == "female" && result == 1) {
+      alert(females[1]);
+    } else if (gender == "female" && result == 2) {
+      alert(females[2]);
+    } else if (gender == "female" && result == 3) {
+      alert(females[3]);
+    } else if (gender == "female" && result == 4) {
+      alert(females[4]);
+    } else if (gender == "female" && result == 5) {
+      alert(females[5]);
+    } else if (gender == "female" && result == 6) {
+      alert(females[6]);
+    } else {
+      alert("null");
+    }
+
 }
