1df125b594e0e59524bc33e0bd684fd06fc0d3af
wain-aina <jesse.wainaina@student.moringaschool.com>
2021-06-11 18:23:07 +0300

    Strict javascript

--- static/js/index.js
+++ static/js/index.js
@@ -10,33 +10,33 @@ function dateakan() {
   var result=day(cc,yy,mm,dd).toFixed();
   var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  if (gender == "male" && result == 0) {
+  if (gender === "male" && result === 0) {
       alert("Your akan name is " + males[0]);
-    } else if (gender == "male" && result == 1) {
+    } else if (gender === "male" && result === 1) {
       alert("Your akan name is " + males[1]);
-    } else if (gender == "male" && result == 2) {
+    } else if (gender === "male" && result === 2) {
       alert("Your akan name is " + males[2]);
-    } else if (gender == "male" && result == 3) {
+    } else if (gender === "male" && result === 3) {
       alert("Your akan name is " + males[3]);
-    } else if (gender == "male" && result == 4) {
+    } else if (gender === "male" && result === 4) {
       alert("Your akan name is " + males[4]);
-    } else if (gender == "male" && result == 5) {
+    } else if (gender === "male" && result === 5) {
       alert("Your akan name is " + males[5]);
-    } else if (gender == "male" && result == 6) {
+    } else if (gender === "male" && result === 6) {
       alert("Your akan name is " + males[6]);
-    } else if (gender == "female" && result == 0) {
+    } else if (gender === "female" && result === 0) {
       alert("Your akan name is " + females[0]);
-    } else if (gender == "female" && result == 1) {
+    } else if (gender === "female" && result === 1) {
       alert("Your akan name is " + females[1]);
-    } else if (gender == "female" && result == 2) {
+    } else if (gender === "female" && result === 2) {
       alert("Your akan name is " + females[2]);
-    } else if (gender == "female" && result == 3) {
+    } else if (gender === "female" && result === 3) {
       alert("Your akan name is " + females[3]);
-    } else if (gender == "female" && result == 4) {
+    } else if (gender == "female" && result === 4) {
       alert("Your akan name is " + females[4]);
-    } else if (gender == "female" && result == 5) {
+    } else if (gender === "female" && result === 5) {
       alert("Your akan name is " + females[5]);
-    } else if (gender == "female" && result == 6) {
+    } else if (gender === "female" && result === 6) {
       alert("Your akan name is " + females[6]);
     } else {
       alert("invalid. Please try again and enter all details");
